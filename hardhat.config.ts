265838f68ca15c9266d303754c06bce13466e3fd
Ingaaa <106094638+ShinyDCL@users.noreply.github.com>
2022-08-14 12:06:52 +0300

    feat: add deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,22 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import '@nomiclabs/hardhat-ethers';
+import 'dotenv/config';
 
 // This adds support for typescript paths mappings
 import 'tsconfig-paths/register';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.STAGING_INFURA_URL,
+      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
