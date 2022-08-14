ac4ba3df70976688463dd90cfb15338dec02194f
Ingaaa <106094638+ShinyDCL@users.noreply.github.com>
2022-08-14 12:13:56 +0300

    chore: clean up

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,6 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
-import '@nomiclabs/hardhat-ethers';
 import 'dotenv/config';
-
-// This adds support for typescript paths mappings
-import 'tsconfig-paths/register';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
